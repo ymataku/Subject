cf7c7059618b44c9d2603842c94d020a4055bd1a
yama <s256282@wakayama-u.ac.jp>
2022-02-28 02:39:34 +0900

    make it better

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -45,20 +45,19 @@ class DocumentController < ApplicationController
   end
 
   def destroy 
-    document = Document.find(params[:id])
-    document.destroy
+    document = Document.where(id:params[:id])
+    document.destroy_all
     redirect_to document_index_path(content:params[:content],pagenation:params[:pagenation])
   end
 
   def edit
     @document = Document.find(params[:id])
-    
   end
 
   def update 
     @document = Document.find(params[:id])
     if @document.update(document_params)
-      redirect_to document_path(@document), notice: "更新しました。"
+      redirect_to document_path(@document,content:params[:content]), notice: "更新しました。"
     end
   end
 
