ed8bbe751dc75844455c7262ab5746740a434364
ymataku <takuya.yt@outlook.jp>
2022-10-11 13:42:36 +0900

    use rubocop

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -1,30 +1,30 @@
 class DocumentController < ApplicationController
-  before_action:authenticate_user!
-  
+  before_action :authenticate_user!
+
   def index
     parameter = params[:group]
     @pagenation = params[:pagenation].to_i
     @parameter = params[:content]
     @params = parameter
-    
-    if parameter == "difficult"
-      @document = Document.where(username:current_user.name).and(Document.where(subjectname: params[:content]).and(Document.where(difficulty:5).or(Document.where(difficulty:4))))
-    elsif parameter == "medium"
-      @document = Document.where(username:current_user.name).and(Document.where(subjectname: params[:content]).and(Document.where(difficulty:3)))
-    elsif parameter == "easy"
-      @document = Document.where(username:current_user.name).and(Document.where(subjectname: params[:content]).and(Document.where(difficulty:2).or(Document.where(difficulty:1))))
-    elsif parameter == "all"
-      @document = Document.where(username:current_user.name).and(Document.where(subjectname: params[:content]))   
+
+    if parameter == 'difficult'
+      @document = Document.where(username: current_user.name).and(Document.where(subjectname: params[:content]).and(Document.where(difficulty: 5).or(Document.where(difficulty: 4))))
+    elsif parameter == 'medium'
+      @document = Document.where(username: current_user.name).and(Document.where(subjectname: params[:content]).and(Document.where(difficulty: 3)))
+    elsif parameter == 'easy'
+      @document = Document.where(username: current_user.name).and(Document.where(subjectname: params[:content]).and(Document.where(difficulty: 2).or(Document.where(difficulty: 1))))
+    elsif parameter == 'all'
+      @document = Document.where(username: current_user.name).and(Document.where(subjectname: params[:content]))
     else
-      @document = Document.where(username:current_user.name).and(Document.where(subjectname: params[:content]))      
+      @document = Document.where(username: current_user.name).and(Document.where(subjectname: params[:content]))
     end
-    if @document.count < (@pagenation-1)*12
-      @pagenation = @pagenation - 1
-      
+    if @document.count < (@pagenation - 1) * 12
+      @pagenation -= 1
+
     elsif @pagenation <= 0
       @pagenation = 1
     end
-    
+
     print(@document.to_json)
   end
 
@@ -38,41 +38,37 @@ class DocumentController < ApplicationController
 
   def create
     document = Document.create(document_params)
-    if document.title == ""
-      document.title = "no title"
-    end
+    document.title = 'no title' if document.title == ''
 
-    
     if document.save
-      redirect_to document_path(document,content:params[:content]),notice:"作成しました."
+      redirect_to document_path(document, content: params[:content]), notice: '作成しました.'
     else
       render :new
     end
   end
 
-  def destroy 
-    document = Document.where(id:params[:id])
+  def destroy
+    document = Document.where(id: params[:id])
     document.destroy_all
-    redirect_to document_index_path(content:params[:content],pagenation:params[:pagenation])
+    redirect_to document_index_path(content: params[:content], pagenation: params[:pagenation])
   end
 
   def edit
     @document = Document.find(params[:id])
   end
 
-  def update 
+  def update
     @document = Document.find(params[:id])
     if @document.update(document_params)
-      redirect_to document_path(@document,content:params[:content]), notice: "更新しました。"
+      redirect_to document_path(@document, content: params[:content]), notice: '更新しました。'
     else
       render :edit
     end
   end
 
-
   private
+
   def document_params
-    params.require(:document).permit(:username,:subjectname,:title,:content,:difficulty)
-  
+    params.require(:document).permit(:username, :subjectname, :title, :content, :difficulty)
   end
 end
--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -1,45 +1,39 @@
 class SubjectController < ApplicationController
-  before_action:authenticate_user!
+  before_action :authenticate_user!
 
-  
   def index
-    
     @parameter = params[:group]
-    if @parameter == ""
-      @parameter = "all"
-    end
+    @parameter = 'all' if @parameter == ''
 
     @page_param = params[:pagenation]
-    if @parameter == "is"
-      @subject = Subject.where(username:current_user.name).and(Subject.where(test:0))
-    elsif @parameter == "notis"
-      @subject = Subject.where(username:current_user.name).and(Subject.where(test:1))
-    elsif @parameter == "all"
-      @subject = Subject.where(username:current_user.name)
-    else
-      @subject = Subject.where(username:current_user.name)      
-    end
+    @subject = if @parameter == 'is'
+                 Subject.where(username: current_user.name).and(Subject.where(test: 0))
+               elsif @parameter == 'notis'
+                 Subject.where(username: current_user.name).and(Subject.where(test: 1))
+               elsif @parameter == 'all'
+                 Subject.where(username: current_user.name)
+               else
+                 Subject.where(username: current_user.name)
+               end
     @pagenumber = (@subject.count / 12).to_i
     @pagenation = @page_param.to_i
 
-    if @subject.count < (@pagenation-1)*12
-      @pagenation = @pagenation - 1
-    
+    if @subject.count < (@pagenation - 1) * 12
+      @pagenation -= 1
+
     elsif @pagenation <= 0
       @pagenation = 1
     end
   end
-  
 
   def new
     @subject = Subject.new
-  
   end
-  
+
   def create
     @subject = Subject.create(subject_params)
     if @subject.save!
-      redirect_to document_new_path(content:@subject.subjectname),notice:"作成しました."
+      redirect_to document_new_path(content: @subject.subjectname), notice: '作成しました.'
     else
       render :new
     end
@@ -47,31 +41,27 @@ class SubjectController < ApplicationController
 
   def edit
     @subject = Subject.find(params[:id])
-    
   end
 
   def update
     subject = Subject.find(params[:id])
     if subject.update(subject_params)
-      redirect_to root_path ,notice:"更新しました"
+      redirect_to root_path, notice: '更新しました'
     else
-      render action: :edit ,notice:"変更に誤りがあります。"
+      render action: :edit, notice: '変更に誤りがあります。'
     end
-
   end
 
   def destroy
     subject = Subject.find(params[:id])
-    Document.where(username:subject.username).and(Document.where(subjectname:subject.subjectname)).destroy_all
+    Document.where(username: subject.username).and(Document.where(subjectname: subject.subjectname)).destroy_all
     subject.destroy
     redirect_to root_path
-    
-
   end
 
   private
-  def subject_params
-    params.require(:subject).permit(:username,:subjectname,:test)
-  end
 
+  def subject_params
+    params.require(:subject).permit(:username, :subjectname, :test)
+  end
 end
